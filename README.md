7fde4f69a23d0086b5180e3d57c3535fcaa22ed0
SteffyCheuk <sc2354@cornell.edu>
2020-04-02 17:37:29 -0700

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# steffycheuk.github.io
---
+++ precache-manifest.8b991cf7c5ab99c0ee1afcd679897b0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "403b1834dfc19fb96f5a29bfa81a43fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "645f067984ed18511a77",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6865601ea3bd490ca5df",
+    "url": "/static/js/2.c82b9f34.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c82b9f34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "645f067984ed18511a77",
+    "url": "/static/js/main.4336fd17.chunk.js"
+  },
+  {
+    "revision": "f04f7a8e91d7810f8d4e",
+    "url": "/static/js/runtime-main.32b06e1f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
